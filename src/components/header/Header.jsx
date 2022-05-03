20c1a1953ad5eb7da95115afce7b885f4ef4a3dc
Donnie Alimov <47795746+mralimov@users.noreply.github.com>
2022-05-03 13:40:33 -0400

    Search bar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,77 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import './header.css';
+import {
+  faBed,
+  faPlane,
+  faCar,
+  faTaxi,
+  faPerson,
+} from '@fortawesome/free-solid-svg-icons';
+import { faCalendarDays } from '@fortawesome/free-regular-svg-icons';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className='header'>
+      <div className='headerContainer'>
+        <div className='headerList'>
+          <div className='headerListItem active'>
+            <FontAwesomeIcon icon={faBed} />
+            <span>Stays</span>
+          </div>
+          <div className='headerListItem '>
+            <FontAwesomeIcon icon={faPlane} />
+            <span>Stays</span>
+          </div>
+          <div className='headerListItem'>
+            <FontAwesomeIcon icon={faCar} />
+            <span>Car rentals</span>
+          </div>
+          <div className='headerListItem'>
+            <FontAwesomeIcon icon={faBed} />
+            <span>Attractions</span>
+          </div>
+          <div className='headerListItem'>
+            <FontAwesomeIcon icon={faTaxi} />
+            <span>Airpot taxis</span>
+          </div>
+        </div>
+        <h1 className='headerTitle'>A lifetime of discounts? It's Genius.</h1>
+        <p className='headerDesc'>
+          Get rewarded for your travels - unlock instant savings of 10% or more
+          with a free Mybooking account
+        </p>
+        <button className='headerBtn'>Sign in / Register</button>
+        <div className='headerSearch'>
+          <div className='headerSearchItem'>
+            <FontAwesomeIcon icon={faBed} className='headerIcon' />
+            <input
+              type='text'
+              placeholder='where are you going?'
+              className='headerSearchInput'
+            />
+          </div>
+          <div className='headerSearchItem'>
+            <FontAwesomeIcon icon={faCalendarDays} className='headerIcon' />
+            <span
+              type='text'
+              placeholder='where are you going?'
+              className='headerSearchInput'
+            >
+              date to date
+            </span>
+          </div>
+          <div className='headerSearchItem'>
+            <FontAwesomeIcon icon={faPerson} className='headerIcon' />
+            <span className='headerSearchText'>2 adults 2 children 1 room</span>
+          </div>
+          <div className='headerSearchItem'>
+            <button className='headerBtn'>Search</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Header from '../../components/header/Header';
 import Navbar from '../../components/navbar/Navbar';
 import './home.css';
 
 const Home = () => {
-  return <Navbar>Navbar</Navbar>;
+  return (
+    <div>
+      <Navbar>Navbar</Navbar>
+      <Header></Header>
+    </div>
+  );
 };
 
 export default Home;
