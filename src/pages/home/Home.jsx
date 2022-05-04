c6d7a2f8b9118999aa6ecf41df38394e4391a42a
Donnie Alimov <47795746+mralimov@users.noreply.github.com>
2022-05-04 16:37:19 -0400

    Featured component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Featured from '../../components/featured/Featured';
 import Header from '../../components/header/Header';
 import Navbar from '../../components/navbar/Navbar';
 import './home.css';
@@ -6,8 +7,11 @@ import './home.css';
 const Home = () => {
   return (
     <div>
-      <Navbar>Navbar</Navbar>
+      <Navbar></Navbar>
       <Header></Header>
+      <div className='homeContainer'>
+        <Featured />
+      </div>
     </div>
   );
 };
